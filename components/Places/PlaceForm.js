277350478eff267c833838bd295577260bfeb4cf
ASUS <eb.mehrdad@gmail.com>
2022-04-25 12:52:28 +0430

    image picker file created

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,21 +1,45 @@
-import { View, Text , ScrollView, TextInput} from 'react-native'
-import React from 'react'
-import { TextInput } from 'react-native-web'
+import { View, Text, ScrollView, TextInput, TextInput } from "react-native";
+import React from "react";
+import { Colors } from " ../../constants/colors";
 
 export default function PlaceForm() {
-  
-  const [enteredTitle, seEntereditle] = useState('')
+  const [enteredTitle, seEntereditle] = useState("");
 
   function changeTitleHandler(enteredText) {
-    seEntereditle(enteredText)
+    seEntereditle(enteredText);
   }
-  
+
   return (
-    <ScrollView>
+    <ScrollView style={styles.form}>
       <View>
-        <Text>PlaceForm</Text>
-        <TextInput  onChangeText={changeTitleHandler} value={enteredTitle}  />
+        <Text style={styles.label}>PlaceForm</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+        />
       </View>
-  </ScrollView>
-  )
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 15,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 5,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 10,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    boarderBottoColor: Colors.primary500,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
