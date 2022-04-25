b8f603f697d8e82109c69648e2588dbaa699c8c1
ASUS <eb.mehrdad@gmail.com>
2022-04-25 12:31:06 +0430

    PlcaeForm created

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text , ScrollView, TextInput} from 'react-native'
 import React from 'react'
+import { TextInput } from 'react-native-web'
 
 export default function PlaceForm() {
+  
+  const [enteredTitle, seEntereditle] = useState('')
+
+  function changeTitleHandler(enteredText) {
+    seEntereditle(enteredText)
+  }
+  
   return (
-    <View>
-      <Text>PlaceForm</Text>
-    </View>
+    <ScrollView>
+      <View>
+        <Text>PlaceForm</Text>
+        <TextInput  onChangeText={changeTitleHandler} value={enteredTitle}  />
+      </View>
+  </ScrollView>
   )
 }
