f9c1bcb357a321a07c50499178202c7a174c013b
ASUS <eb.mehrdad@gmail.com>
2022-04-26 12:58:21 +0430

    style improved

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -4,26 +4,46 @@ import {launchCameraAsync} from 'expo-image-picker'
 
 
 function ImagePicker() {
-    
+      const [pickedImage, setPickedImage] = useState();
    async function takeImageHandler(){
      const image = await launchCameraAsync({
        allowsEditing: true,
        aspect: [16, 9],
        quality: 0.5
         
-    })
-    console.log(image)
+     })
+      setPickedImage(image.uri); // image.uri is the uri of the image we just took with the camera
    }
    
- }
-
-export default function ImagePicker() {
+  let imagePreview = <Text> no image</Text>
+  if (pickedImage) {
+    imagePreview = <Image style={s.image} source={{ uri: pickedImage }} />
+  }
+  
   return (
     <View>
-      <View>
-         <Image source={{}}
+      <View  style={s.imagePreview}>
+         {imagePreview}
       </View>
       <Button title="Take Image" onPress={takeImageHandler} />
     </View>
   )
+  
 }
+  
+const s = StyleSheet.create({
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: lightBlue
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+
+})
+export default ImagePicker;
